1990d9865f7b838d6eaa5c0863a5cb25c1986cd3
Lior Mualem <liorm@users.noreply.github.com>
2019-04-17 20:02:47 +0300

    updated java download path

--- elasticsearch-vmss/install-elasticsearch.sh
+++ elasticsearch-vmss/install-elasticsearch.sh
@@ -95,7 +95,7 @@ done
 # Install Oracle Java
 install_java()
 {
-    if [ -f "jdk-8u212-linux-x64.tar.gz" ];
+    if [ -f "jdk-8u201-linux-x64.tar.gz" ];
     then
         log "Java already downloaded"
         return
@@ -105,8 +105,8 @@ install_java()
     RETRY=0
     MAX_RETRY=5
     while [ $RETRY -lt $MAX_RETRY ]; do
-        log "Retry $RETRY: downloading jdk-8u212-linux-x64.tar.gz"
-        wget --no-check-certificate --no-cookies --header "Cookie: oraclelicense=accept-securebackup-cookie" https://download.oracle.com/otn/java/jdk/8u212-b10/59066701cf1a433da9770636fbc4c9aa/jdk-8u212-linux-x64.tar.gz
+        log "Retry $RETRY: downloading jdk-8u201-linux-x64.tar.gz"
+        wget --continue --no-check-certificate --no-cookies https://github.com/frekele/oracle-java/releases/download/8u201-b09/jdk-8u201-linux-x64.tar.gz
         if [ $? -ne 0 ]; then
             let RETRY=RETRY+1
         else
@@ -114,12 +114,12 @@ install_java()
         fi
     done
     if [ $RETRY -eq $MAX_RETRY ]; then
-        log "Failed to download jdk-8u212-linux-x64.tar.gz"
+        log "Failed to download jdk-8u201-linux-x64.tar.gz"
         exit 1
     fi
     
-    tar xzf jdk-8u212-linux-x64.tar.gz -C /var/lib
-    export JAVA_HOME=/var/lib/jdk1.8.0_212
+    tar xzf jdk-8u201-linux-x64.tar.gz -C /var/lib
+    export JAVA_HOME=/var/lib/jdk1.8.0_201
     export PATH=$PATH:$JAVA_HOME/bin
     log "JAVA_HOME: $JAVA_HOME"
     log "PATH: $PATH"
